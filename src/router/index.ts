86d940dbbad8e5edc9196b7d3c64f31b4e287986
Ankit Rai <aviii@Aviiis-MacBook-Air.local>
2025-08-19 22:09:04 +0530

    add: create order route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import Listing from '../components/Listing.vue'
+import Create from '../components/Create.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Listing',
     component: Listing,
+  },
+    {
+    path: '/create',
+    name: 'Create',
+    component: Create,
   },
 ]
 
